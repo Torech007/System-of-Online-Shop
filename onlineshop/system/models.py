27d4f5cb04289018370e423702714e4dfbcbc72e
Torech007 <skerbektrebek@gmail.com>
2025-04-09 11:39:23 +0500

    superuser

--- onlineshop/system/models.py
+++ onlineshop/system/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class system(models.Model):
+    title = models.CharField(max_length=255) # Название задачи
+    description = models.TextField(blank=True, null=True) # Описание
+    created_at = models.DateTimeField(auto_now_add=True) # Дата создания
+    
+    def __str__(self):
+        return self.title
